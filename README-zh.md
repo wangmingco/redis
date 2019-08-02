a848ecd9631dd29593f8817480d56e21e69205c8
wangming <wangming15@hotmail.com>
2019-08-02 11:09:59 +0800

    添加clon配置

---
+++ README-zh.md
@@ -0,0 +1,3 @@
+配置教程:
+
+http://beautyboss.me/2019/03/10/CLion%E8%B0%83%E8%AF%95Redis5%E6%BA%90%E7%A0%81/
---
+++ src/release.h
@@ -0,0 +1,3 @@
+#define REDIS_GIT_SHA1 "239069de"
+#define REDIS_GIT_DIRTY "      13"
+#define REDIS_BUILD_ID "wangmingdeMacBook-Pro.local-1564714561"
